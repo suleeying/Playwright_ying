83f6e3d8c90b9d598070edee9d748ecbe518c6cc
suleemas <suleemas.fua@fullteam.tech>
2025-08-08 17:24:57 +0700

    edit

--- pos_cms/create_terminal.spec.ts
+++ pos_cms/create_terminal.spec.ts
@@ -1,27 +1,20 @@
 import { test, expect } from '@playwright/test';
-import { login } from '../config';
+import { login,open_brand_and_outlet } from '../config';
 const URL = 'https://staging-bluestat-cms.devfullteam.tech/login';
 const EMAIL = 'suleemas.fua+55@fullteam.tech';
 const PASSWORD = 'Ying964232';
 const BRAND_ID = "607"; //ying_BS_onboardไม่มีเพดาน
 const OUTLET_ID = "1607"; //1607
 
-
 test.describe('Login', () => {
   test('create and delete terminal', async ({ page }) => {
   await login(page, URL, EMAIL, PASSWORD);
-  await page.waitForLoadState('networkidle');
-  await page.waitForTimeout(5000);
-  await page.getByText('ผู้ใช้งานสูงสุด').first().click();
-  await page.getByPlaceholder('ค้นหาแบรนด์..').click();
-  await page.getByPlaceholder('ค้นหาแบรนด์..').fill(BRAND_ID);
-  await page.getByText(BRAND_ID).click();
-  await page.getByText('เปิด').click();
+  await open_brand_and_outlet(page, BRAND_ID, OUTLET_ID);
   await page.getByText('POS ทั้งหมด').click();
   await page.waitForTimeout(3000);
 
   //เช็คว่า column 'Terminal ID' แสดงอยู่หรือไม่
-  const terminalIdColumn = page.getByText('Terminal ID');
+  const terminalIdColumn = page.getByText('Terminal ID');// ใช้ getByText เพื่อค้นหา column 'Terminal ID'
   if (await terminalIdColumn.isVisible()) {// ถ้า column 'Terminal ID' แสดงอยู่
     console.log('Terminal ID column is visible');//
     const serialno = await page.locator('#row-0 > div').nth(4).innerText();
@@ -32,9 +25,22 @@ test.describe('Login', () => {
       await page.waitForTimeout(3000); 
       await page.getByRole('button', { name: '' }).click();
       await page.getByRole('button', { name: 'confirm' }).click();
-      console.log('ลบ terminal สำเร็จ');  
+      console.log('ลบ terminal สำเร็จ');
+      await page.waitForTimeout(3000);  
+      // สร้าง terminal ต่อหลังลบสำเร็จ
+      await page.getByRole('link', { name: 'สร้าง' }).click();
+      await page.locator('#outletId').selectOption(OUTLET_ID);
+      await page.getByRole('textbox', { name: 'Activate Code : ชื่อเครื่อง' }).click();
+      await page.getByRole('textbox', { name: 'Activate Code : ชื่อเครื่อง' }).fill('test');
+      await page.locator('span').filter({ hasText: 'POS BDG' }).locator('i').click();
+      await page.waitForTimeout(2000);
+      await page.getByRole('button', { name: 'บันทึก' }).click();
+      await page.waitForTimeout(2000);
+      const newserialno = await page.locator('#row-0 > div').nth(4).innerText();
+      console.log('สร้าง terminal สำเร็จ '+'Serial No:', newserialno);
     }
   } else {
+    // กรณีไม่มี terminal ให้สร้างใหม่เลย
     await page.getByRole('link', { name: 'สร้าง' }).click();
     await page.locator('#outletId').selectOption(OUTLET_ID);
     await page.getByRole('textbox', { name: 'Activate Code : ชื่อเครื่อง' }).click();
