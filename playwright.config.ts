b5ed2ae995a40025ac209aa9bb09e735038d302a
suleemas <suleemas.fua@fullteam.tech>
2025-09-22 10:56:23 +0700

    edit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './pos_cms',
+  testDir: './automate',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
